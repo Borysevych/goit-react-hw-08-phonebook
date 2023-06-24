8d93dc9df96b5d6a3effc6d5214565717896e00d
Borysevych <borysevych.rus@gmail.com>
2023-06-24 17:47:43 +0300

    React HW 8
    make react HW 8

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import { HomeTitle, WrapperHomePage } from './HomePage.styled';
+
+const HomePage = () => {
+  return (
+    <WrapperHomePage>
+      <HomeTitle>
+        Please authorize and use Phone book!
+      </HomeTitle>
+    </WrapperHomePage>
+  );
+};
+
+export default HomePage;
---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,8 @@
+export const NotFoundPage = () => {
+  return (
+    <>
+      <h3>Error 404</h3>
+      <p>Sorry! This page doesn't exist!</p>
+    </>
+  );
+};
